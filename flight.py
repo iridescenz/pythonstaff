1ce97a28a5f77e2c53d45c472bc273f136942d10
Irina Turenko <turenko7irina@mail.ru>
2020-07-09 17:41:37 +0300

    a

---
+++ flight.py
@@ -0,0 +1,24 @@
+
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+
+flight = Flight(3)
+
+people = ["Ira", "Masha", "Danil", "Mila"]
+for person in people:
+    if flight.add_passenger(person):
+        print(f"We added {person} successfully")
+    else:
+        print(f"No available seats for {person}")
